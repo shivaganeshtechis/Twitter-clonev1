91c424374bc377f108b701dbb1b6a32a52379f3d
shivaganeshtechis <shivaganesh@techis.io>
2022-09-22 10:12:56 +0530

    updated settings.py

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 X_FRAME_OPTIONS = '*'
 CORS_ORIGIN_ALLOW_ALL = True
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000/']
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000/','http://0.0.0.0:3000/','https://Twitter-clonev1-1.shivaganeshtech.repl.co']
 
 
 # Application definition
